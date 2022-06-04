6252221645a8bd31c49fabc8a02efa273dbf9159
Jicaffo <jicaffo@yahoo.com>
2022-06-04 16:50:59 -0300

    morgan, dotenv & cors added

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-//import 'dotenv/config';
+import 'dotenv/config';
 import express from 'express';
-//import morgan from 'morgan';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
+const userName = process.env.NAME;
 
 
+// Settings
+app.set("port", PORT);
+
+
+// Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+
+
+// Routes
 app.use("/", (req, res) =>{
     res.status(200).json({
-        msg: `Petition recieved on route '${req.originalUrl}'`,
+        msg: `Hi, ${userName}! Petition recieved on route '${req.url}'`,
     })
 })
 
-app.listen(PORT, () => console.log(`*** App listening on port ${PORT} ***`))
+
+// Starting Server
+app.listen(app.get("port"), () => console.log(`*** App listening on port ${app.get("port")} ***`));
