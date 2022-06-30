722dd8320c7c4462768967a02882f8b02d048d2f
Jicaffo <jicaffo@yahoo.com>
2022-06-30 12:43:20 -0300

    Body parser + returning body through console

--- index.js
+++ index.js
@@ -29,12 +29,14 @@ app.set("port", PORT);
 
 
 // Middlewares
+app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
 
 // Routes
 app.get("/", async (req, res) =>{
+    if (req.body) console.log("body object recieved: ", req.body);
     res.status(200).json({
         msg: `Hi, ${userName}! This VERY SIMPLE app just shows your name and the subroute you are in. For example, in this case you are at: '${req.url}'. Try another route!`,
         saltedHashName: await hashValue(userName),
@@ -52,6 +54,7 @@ app.get("/", async (req, res) =>{
 })
 
 app.use("/", async (req, res) =>{
+    if (req.body) console.log("body object recieved: ", req.body);
     res.status(200).json({
         msg: `Hi, ${userName}! This ${req.method} request was recieved at route '${req.url}'`,
         saltedHashName: await hashValue(userName),
